87c246f32787bc6696a2b32a7765f058b72a4ae2
antoniotrrs <luisato.trrs@gmail.com>
2025-04-18 19:54:25 +0100

    Profile component created
    update vuetify implementation and profile component created

---
+++ src/constants/theme.js
@@ -0,0 +1,11 @@
+export const profileConstants = {
+  name: 'Antonio Torres',
+  job: 'iOS Developer',
+  title: 'Hello World!',
+  description: 'This is Tony. I\'ve worked on a variety of technology challenges from fields such as medicine, retail and logistic. Having strong knowledge in mobile and web development using Swift, Python, PHP, Javascript, HTML5 to achieve the goals required from companies. Focused on Data Analytics and AI-driven models aim to provide Intelligent Portable Software. LET\'S WORK TOGETHER!😉',
+  socialLinks: [
+    { icon: 'mdi-github', url: 'https://github.com/antoniotrrs' },
+    { icon: 'mdi-linkedin', url: 'https://linkedin.com/in/antoniotrrs' },
+    { icon: 'mdi-apple', url: 'https://twitter.com/yourusername' },
+  ],
+}
