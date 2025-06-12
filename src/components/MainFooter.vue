b12705d53b4de3ba483b4da53214dc333e5ff92f
antoniotrrs <luisato.trrs@gmail.com>
2025-06-12 13:28:20 +0100

    Updated footer

---
+++ src/components/MainFooter.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-footer class="d-flex align-center justify-center ga-2 flex-wrap flex-grow-1 py-3" color="surface-light">
+    
+    <div class="flex-1-0-100 text-center mt-2">
+        <v-row>
+            <v-col cols="12" sm="4">
+                <h5>Antonio</h5>
+                <a href="/" class="text-decoration-none text-black" style="font-size: 14px;">Home</a><br>
+                <a href="/about" class="text-decoration-none text-black" style="font-size: 14px;">Resume</a><br>
+                <a href="/apps" class="text-decoration-none text-black" style="font-size: 14px;">Apps</a>
+            </v-col>
+            <v-col cols="12" sm="4">
+                <h5>Contact</h5>
+                <a href="mailto:hi.tonytorres@gmail.com" style="font-size: 14px; color: black; text-decoration: none;">hi.tonytorres@gmail.com</a>
+            </v-col>
+            <v-col cols="12" sm="4">
+                <h5>Tony Online</h5>
+                <v-btn
+                  v-for="link in socialLinks"
+                  :key="link.icon"
+                  :href="link.url"
+                  target="_blank"
+                  class="mx-1"
+                  variant="plain">
+                    <v-icon size="large">{{ link.icon }}</v-icon>
+                  </v-btn>
+            </v-col>
+        </v-row>
+      <h5>Antonio Torres © {{ new Date().getFullYear() }} | Powered by Vue.js</h5>
+    </div>
+  </v-footer>
+</template>
+
+<script setup>
+import { profileConstants } from '@/constants/theme.js'
+
+const { socialLinks } = profileConstants
+
+</script>
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,15 +1,15 @@
 <template>
 
   
-    <v-container fluid class=" position-relative">
-      <v-row class="position-absolute left-0 right-0 top-mobile" style="min-height: 40rem;">
+    <v-container fluid class=" position-relative height-parent">
+      <v-row class="position-absolute left-0 right-0 top-mobile ma-0" style="min-height: 45rem;">
         <v-col cols="12" sm="5" style="background-color: #daeced;"></v-col>
       </v-row>
-      <v-row class="position-absolute w-100" justify="center">
+      <v-row class="position-absolute ma-0 mt-5" justify="center">
         <v-col cols="10" sm="5" md="4" lg="3" class="text-center">
           <v-card class="pa-5 rounded-0 d-flex flex-column align-center" elevation="6" >
               <v-avatar size="200" class="mx-auto my-5">
-                <v-img src="img/tony_torres.jpg" alt="Antonio Torres" />
+                <v-img src="img/tony_torres2.jpeg" alt="Antonio Torres" />
               </v-avatar>
 
               <h2 class="my-5">{{ name }}</h2>
@@ -19,7 +19,8 @@
                 color="primary"
                 width="50"
               ></v-divider>
-              <h3 class="my-5 font-weight-regular">{{ job }}</h3>
+              <h4 class="mt-5 font-weight-regular">{{ job }}</h4>
+              <h4 class="font-weight-regular">📍 Dublin, Ireland</h4>
               <div class="mt-5 mb-3">
                 <v-btn
                   v-for="link in socialLinks"
@@ -36,7 +37,7 @@
             <v-col cols="10" md="5" sm="5" lg="4" class="d-flex flex-column justify-center">
               <h1 class="text-h3 font-weight-medium">{{ title }}</h1>
               <v-row class="flex-0-0 my-5">
-                <v-col cols="6">
+                <v-col cols="6" sm="4">
                   <v-btn
                 color="primary"
                 href="/doc/webcv_en.pdf"
@@ -46,10 +47,10 @@
                  Resume
                 </v-btn>
                 </v-col>
-                <v-col cols="6">
+                <v-col cols="6" sm="4">
                   <v-btn
                     color="primary"
-                    href="mailto:luisato.trrs@gmail.com"
+                    href="mailto:hi.tonytorres@gmail.com"
                     target="_blank"
                     variant="outlined"
                     rounded="xl"
@@ -58,17 +59,12 @@
                 </v-btn>
                 </v-col>
               </v-row>
-              <div v-html="aboutme"></div>
+              <div style="font-size: 15px;" v-html="aboutme"></div>
           </v-col>
         </v-row>
       
     </v-container>
 
-  
-
-
-
- 
 </template>
 
 <script setup>
@@ -83,9 +79,17 @@ const aboutme = description.replace(/\n/g, '<br>');
   top: auto; /* default for non-mobile */
 }
 
+.height-parent {
+  min-height: 45rem;
+}
+
 @media (max-width: 600px) {
   .top-mobile {
     top: 400px;
+  }
+
+  .height-parent {
+    min-height: calc(400px + 700px);
   }
 }
 </style>
